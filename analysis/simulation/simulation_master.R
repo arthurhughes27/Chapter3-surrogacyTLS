39941a7e84bc595fe327591962720e3be3a491b2
Arthur Hughes <arthur.hughes27@outlook.com>
2026-08-24 17:54:08 +0200

    garbage collection

--- analysis/simulation/simulation_master.R
+++ analysis/simulation/simulation_master.R
@@ -1,6 +1,15 @@
 # Master script to run all main-text simulation studies in order.
 
 source(fs::path("analysis", "simulation", "fpr_vs_sample_size.R"))
+
+gc()
 source(fs::path("analysis", "simulation", "fpr_vs_correlation.R"))
+
+gc()
 source(fs::path("analysis", "simulation", "power_fdr_vs_surrogate_strength.R"))
+
+gc()
+
 source(fs::path("analysis", "simulation", "gamma_pvalue_vs_prop_invalid.R"))
+
+gc()
